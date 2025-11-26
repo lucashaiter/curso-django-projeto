1ab29cb68a6e2f150e2de94133c91642693c945a
lucashaiter <lucas.leoni.haiter@gmail.com>
2025-11-25 21:25:29 -0300

    real data & querysets

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from .models import Recipe, Category
 
-# HTTP Request
+
 def home(request):
-    # return HTTP Response
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)]
+        'recipes': recipes
     })
 
-def recipe(request, id):
+def category(request, category_id):
+    categories = Recipe.objects.filter(category__id = category_id).order_by('-id') 
+    return render(request, 'recipes/pages/home.html', context={
+        'categories': categories
+    })
+
+def recipe(request, id):    
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': make_recipe(),
         'is_detail_page': True
