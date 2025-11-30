339f4ea020b16924e10b458c8ce2e97422057fc7
lucashaiter <lucas.leoni.haiter@gmail.com>
2025-11-30 14:17:08 -0300

    http404

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
+from django.http import Http404
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
-from .models import Recipe, Category
+from recipes.models import Recipe
 
 
 def home(request):
@@ -11,8 +12,13 @@ def home(request):
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(category__id = category_id, is_published = True).order_by('-id') 
+    
+    if not recipes:
+        raise Http404("Not found")
+    
     return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes
+        'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category │ '
     })
 
 def recipe(request, id):    
